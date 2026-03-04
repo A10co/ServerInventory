37dc3578bb07f89f5f57b9c20e13a7a555da695a
Alhamid <qasem.alhamid@gmail.com>
2026-03-04 13:31:14 +0100

    add server creation feature

--- ServerInventory/Program.cs
+++ ServerInventory/Program.cs
@@ -19,10 +19,34 @@
                 Console.WriteLine("4 - Exit");
 
                 string choice = Console.ReadLine();
-
                 if (choice == "1")
                 {
-                    Console.WriteLine("Add Server selected");
+                    Console.Write("Server Name: ");
+                    string name = Console.ReadLine();
+
+                    Console.Write("IP Address: ");
+                    string ip = Console.ReadLine();
+
+                    Console.Write("Operating System: ");
+                    string os = Console.ReadLine();
+
+                    Console.Write("Location: ");
+                    string location = Console.ReadLine();
+
+                    Console.Write("Status: ");
+                    string status = Console.ReadLine();
+
+                    Server newServer = new Server();
+
+                    newServer.Name = name;
+                    newServer.IPAddress = ip;
+                    newServer.OperatingSystem = os;
+                    newServer.Location = location;
+                    newServer.Status = status;
+
+                    servers.Add(newServer);
+
+                    Console.WriteLine("Server added successfully!");
                 }
                 else if (choice == "2")
                 {
