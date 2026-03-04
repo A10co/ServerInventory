1cc6d54df32b5235b14fa1dc37888cde4c65a988
Alhamid <qasem.alhamid@gmail.com>
2026-03-04 12:18:54 +0100

    add basic console menu

--- ServerInventory/Program.cs
+++ ServerInventory/Program.cs
@@ -4,9 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" Server Inventory gestartet!");
+           
+
+            Console.WriteLine("Server Inventory gestartet!");
 
             List<Server> servers = new List<Server>();
+
+            
+            
+                Console.WriteLine("1 - Add Server");
+                Console.WriteLine("2 - Show Servers");
+                Console.WriteLine("3 - Delete Server");
+                Console.WriteLine("4 - Exit");
+
+                string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                Console.WriteLine("Add Server selected");
+            }
+            else if (choice == "2")
+            {
+                Console.WriteLine("Show Servers selected");
+            }
+            else if (choice == "3")
+            {
+                Console.WriteLine("Delete Server selected");
+            }
+            else if (choice == "4")
+            {
+                Console.WriteLine("Exit selected");
+                
+            }
+            
+
+
 
         }
     }
